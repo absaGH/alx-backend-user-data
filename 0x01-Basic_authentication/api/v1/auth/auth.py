09571568bd5835f02d35b3c9fa54f582d6d1af59
absaGH <absa3852@gmail.com>
2022-03-08 11:55:15 +0000

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
-"""
-   This class is the template used for authentication system that will be implemented.
+"""This class is the template used for authentication system that will be implemented
 """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth():
-    """
-       The definition of the class used for
+    """The definition of the class used for
        the authenication system
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-           It checks on the file path and authorization
+        """It checks on the file path and authorization
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
@@ -35,10 +32,12 @@ class Auth():
         return False
 
     def authorization_header(self, request=None) -> str:
-        """authorization function used"""
+        """authorization function used
+        """
         if request is None or request.headers.get('Authorization') is None:
             return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Returns the current user"""
+        """Returns the current user
+        """
         return None
