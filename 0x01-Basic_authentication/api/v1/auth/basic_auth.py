8b9f23a441e7503aa4c1681169d02ec25e6e2cda
absaGH <absa3852@gmail.com>
2022-03-08 13:34:23 +0000

    Basic-Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,19 @@ from typing import Tuple, TypeVar
 class BasicAuth(Auth):
     """Basic Auth class defintion based on Auth
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Returns the Base64 part of the Authorization
+           header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if authorization_header[0:6] != 'Basic ':
+            return None
+
+        return authorization_header[6:]
