3aa168409f9b548e9fc1d9faa585e5f2beec41c2
absaGH <absa3852@gmail.com>
2022-03-09 15:32:49 +0000

    gnerate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,7 +80,7 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
-        """ take email as input, generate a UUID and update the user’s reset_token database
+        """ generate UUID & update user’s reset_token db
         """
         if not session_id:
             return None
