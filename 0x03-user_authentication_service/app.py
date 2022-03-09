a1fc22cd1b332cab37c4b42cb9958f79927b441a
absaGH <absa3852@gmail.com>
2022-03-09 13:10:32 +0000

    Register POST end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,5 +17,21 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """ end-point to register user
+    """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": "{}".format(user.email),
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
