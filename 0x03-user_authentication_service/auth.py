a9ac848ab55633885ef39600b6b8e0a7cb141f40
absaGH <absa3852@gmail.com>
2022-03-09 12:31:34 +0000

    validate and register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,3 +15,24 @@ def _hash_password(password: str) -> str:
     """
     binary_password = bytes(password, "ascii")
     return bcrypt.hashpw(binary_password, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register and return a User object.
+            If a user already exist with the passed email            , raise a ValueError.
+            If not, hash the password with _hash_password            , save the user to the database.
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return user
+        else:
+            raise ValueError("User {} already exists".format(email))
