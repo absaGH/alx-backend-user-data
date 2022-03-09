ee5b117b94795293feb96b7bf5a9233d0e4e0666
absaGH <absa3852@gmail.com>
2022-03-09 14:52:10 +0000

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,5 +52,24 @@ def login() -> str:
     abort(401)
 
 
+@ app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """ Destroye session using session_id
+    """
+    sessionID = request.cookies.get('session_id', None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(sessionID)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/', code=302)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
