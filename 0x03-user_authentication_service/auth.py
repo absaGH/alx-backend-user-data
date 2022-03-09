4ab3ee62959696affc7321771ffeb3f071e28743
absaGH <absa3852@gmail.com>
2022-03-09 14:12:48 +0000

    get sesssion ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,3 +50,15 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Find user, generate new UUID, save it in db and session ID.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            sess_id = _generate_uuid()
+            self._db.update_user((user.id), session_id=sess_id)
+
+            return sess_id
+        except NoResultFound:
+            return None
